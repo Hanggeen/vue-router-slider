4160a7263c299f05d59f1fc2aa2d0c7a6caa8be6
Hanggeen <592499197@qq.com>
2019-03-15 20:52:31 +0800

    更改demo样式，展示更直观

--- src/App.vue
+++ src/App.vue
@@ -28,47 +28,90 @@ export default {
   margin: 0;
   padding: 0;
 }
+input,
+button,
+div {
+  outline: 0;
+}
+
+a,
+img,
+button,
+input,
+textarea,
+div {
+  -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+}
+
+input,
+textarea {
+  -webkit-appearance: none;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #ffffff;
+  color: #2c3e50;
 }
 .content {
-  margin: 5vh 10vw;
-  width: 80vw;
-  height: 90vh;
+  width: 100vw;
+  height: 100vh;
 }
 
+
+/* 子路由样式 */
 .page {
-  text-align: center;
+  font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
   width: 100%;
-  min-height: 100vh;
-  padding-top: 20px;
+  height: 100vh;
+  color: #3e5268;
+  box-shadow: -5px 0px 10px rgba(0,0,0, .1);
+  background-color: #fff;
+  text-align: center;
+  font-size: 14px;
 }
-h4 {
-  color: #ffffff;
+.page > .top {
+  margin-bottom: 20px;
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+  box-shadow: 0px 0px 10px rgba(0,0,0, .1);
 }
-a,p {
-  display: block;
-  color: #fff;
-  font-size: 20px;
-  margin: 10px 10px;
+.page > .top a{
   text-decoration: none;
+  color: #2c3e50;
 }
-.page0 {
-  background-color: hsl(0, 93%, 83%)
+.page > .top .logo {
+  display: inline-block;
+  vertical-align: middle;
+  width: 30px;
+  height: 30px;
+  background-image: url(https://cn.vuejs.org/images/logo.png);
+  background-size: 100% 100%;
 }
-.page1 {
-  background-color: hsl(197, 95%, 83%)
+.page > .top span {
+  vertical-align: middle;
+  font-size: 16px;
+  color: #3e5268;
 }
-.page2 {
-  background-color: hsl(133, 93%, 83%)
+.page > .title > span{
+  margin-left: 20px;
 }
-.page3 {
-  background-color: hsl(55, 85%, 68%)
+.page  > .title{
+  margin-top: 40px;
 }
-.page4 {
-  background-color: hsl(315, 93%, 83%)
+.page > .links > a,
+.title,
+.links > p {
+  display: block;
+  font-size: 14px;
+  margin: 15px 40px;
+}
+.page > .links > a {
+  color: #42b983
+}
+.links > p:last-child {
+  margin-top: 50px;
 }
 </style>
--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="page page1">
-    <h4 @click="$router.go(-1)">返回上一级</h4>
-    <router-link to="/">Page0</router-link>
-    <router-link to="/page2">Page2</router-link>
-    <router-link to="/page3">Page3</router-link>
-    <router-link to="/page4">Page4</router-link>
-    <p>当前堆栈{{slotProps.stack}}</p>
+  <div class="page page0">
+    <div class="top">
+      <a href="https://github.com/Hanggeen/vue-router-slider">
+        <div class="logo"></div>
+        <!-- <img src="https://cn.vuejs.org/images/logo.png" alt=""> -->
+        <span>vue-router-slider</span>
+      </a>
+    </div>
+    <p>Vue单页面路由滑动插件，支持手势返回</p>
+    <p class="title">当前页面：Page1 <span>当前堆栈:{{slotProps.stack}}</span></p>
+    <div class="links">
+      <p>你可以点击下面前往其他page</p>
+      <router-link to="/">Page0</router-link>
+      <p>Page1</p>
+      <router-link to="/page2">Page2</router-link>
+      <router-link to="/page3">Page3</router-link>
+      <router-link to="/page4">Page4</router-link>
+      <p @click="$router.go(-1)" v-if="slotProps.stack !== 0">返回上一级</p>
+    </div>
   </div>
 </template>
 <script>
@@ -13,4 +25,3 @@ export default {
   props: ['slotProps']
 }
 </script>
-
